674474b85e391d8c30ae2d948bd305c5366c36a0
William Lay <willlaydev@gmail.com>
2024-05-10 22:07:26 +1000

    tracks members time in discord channel

--- app.js
+++ app.js
@@ -2,13 +2,54 @@
 const { Client, Events, GatewayIntentBits} = require('discord.js');
 require('dotenv').config()
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent,GatewayIntentBits.GuildVoiceStates] });
+//db
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// When the client is ready, run this code (only once).
-client.once(Events.ClientReady, readyClient => {
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const db = new MongoClient(process.env.DB_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+});
+
+// Create a new client instance
+const bot = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent,GatewayIntentBits.GuildVoiceStates] });
+
+bot.once(Events.ClientReady, readyClient => {
 	console.log(`Bot is in! Logged in as ${readyClient.user.tag}`);
 });
 
-// Log in to Discord with your client's token
-client.login(process.env.BOT_TOKEN);
+let users = new Map();
+
+bot.on('voiceStateUpdate', (oldState, newState) => {
+    const member = oldState.member || newState.member;
+    
+    // If the member was not in a channel before
+    if (oldState.channel === null) {
+        console.log(member.user.displayName + " joined a channel");
+        users.set(member.id, new Date().getTime());
+    } else {
+        // Member left the channel
+        console.log(member.user.displayName + " left a channel");
+        const points = Math.floor((new Date().getTime() - users.get(member.id)) / 1000); // in seconds
+        addPoints(points, member);
+    }    
+})
+
+async function addPoints(points, member) {
+    try {
+    // Check if the user exists in the database
+        db.db("points-db").collection("Users").findOneAndUpdate(
+            { id: member.id },
+            { $inc: {points: points }, $set: {displayName: member.user.displayName}},
+            { returnDocument: false, upsert: true}
+        );
+    } catch (error) {
+        console.error("error updating");
+        db.close()
+    }
+}
+
+bot.login(process.env.BOT_TOKEN);
