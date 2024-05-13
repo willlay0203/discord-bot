c88445fac063abb226af42163abb7b4e7392ac23
William Lay <willlaydev@gmail.com>
2024-05-13 10:41:45 +1000

    add points bug fixed

--- app.js
+++ app.js
@@ -46,14 +46,19 @@ const addPoints = async (currTime, member) => {
     const points = Math.floor(((currTime - users.get(member.id)) / 1000) / 60) * 10;
     try {
         // Check if the user exists in the database
-        const res = await db.db("points-db").collection("Users").findOneAndUpdate(
-            { id: member.id },
-            { $inc: {points: points }, $set: {displayName: member.user.displayName}},
-            { returnDocument: 'after', upsert: true}
-        );
-        console.log(`Added ${points} points to ${res.displayName}`);
+        const user = await db.db("points-db").collection("Users").findOne({_id: member.id});
+        
+        if (user === null) {
+            db.db("points-db").collection("Users").insertOne({_id: member.id, displayName: member.user.displayName, points: points});
+        } else {
+            db.db("points-db").collection("Users").updateOne(
+                { _id: member.id },
+                { $inc: {points: points }}
+            )
+        }
+        console.log(`Added ${points} points to ${member.user.displayName}`);
     } catch (error) {
-        console.error("error updating");
+        console.log(error)
     }
 }
 
