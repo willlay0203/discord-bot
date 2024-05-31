e1709c3861a318deaca1e646b1f152f4f522b6a5
josiahwebster <josiahwebster12@gmail.com>
2024-05-31 14:49:02 +1000

    channel id adjust

--- utils/msg.js
+++ utils/msg.js
@@ -7,7 +7,7 @@ const DEFAULT_CHANNEL_ID = "1239383694033158254";
 /**
  * Use to send a simple string a channel,  default bot channel - social-credit-updates
  */
-export const msgChannel = (message, channelId = TEST_CHANNEL_ID) => {
+export const msgChannel = (message, channelId = DEFAULT_CHANNEL_ID) => {
     const channel = bot.channels.cache.get(channelId);
     channel.send(message);
 }
