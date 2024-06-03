53a256ee64097e97b86989642f43b4b8d783698a
josiahwebster <josiahwebster12@gmail.com>
2024-06-03 23:41:24 +1000

    added remake check

--- features/gamble.js
+++ features/gamble.js
@@ -20,9 +20,9 @@ const checkBet = async (match, predictedResult, userId, member) => {
             if (betOutcome === predictedResult) {
                 return true;
             }  
-            // if (betOutcome === 'remake') {
-            //     return 'remake';
-            // }
+            if (betOutcome === 'remake') {
+                return 'remake';
+            }
             return false;
         }
         // Minute checks
@@ -62,10 +62,10 @@ export const handleBet = async (interaction, match, userId, betAmount) => {
         if (betResult === true) {
             addPoints(betAmount * 2, member);
         }
-        // if (betResult === 'remake') {
-        //     console.log(`${member.user.displayName}'s bet was a remake. Refund ${betAmount} points.`);
-        //     addPoints(betAmount, member);
-        // }
+        if (betResult === 'remake') {
+            console.log(`${member.user.displayName}'s bet was a remake. Refund ${betAmount} points.`);
+            addPoints(betAmount, member);
+        }
         return betResult;
     }
 };
